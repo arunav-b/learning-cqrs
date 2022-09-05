c64ec562714a66e3cefe2ce879fe5f5db6e1cd0c
Arunav Bhattacharya <itsarunav4u@gmail.com>
2022-09-04 19:31:55 -0700

    Uncommented code

--- bank-account/account.cmd/src/main/java/com/techbank/account/cmd/Application.java
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/Application.java
@@ -1,13 +1,12 @@
-//package com.techbank.account.cmd;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class Application {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(Application.class, args);
-//	}
-//
-//}
+package com.techbank.account.cmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
