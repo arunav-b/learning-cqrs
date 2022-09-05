fa8c9be5423fdffd0a2ac9d75c5570027cdca353
Arunav Bhattacharya <itsarunav4u@gmail.com>
2022-09-04 19:56:17 -0700

    Updated gradle plugin

--- bank-account/.idea/sonarlint/issuestore/index.pb
+++ bank-account/.idea/sonarlint/issuestore/index.pb
@@ -5,5 +5,3 @@ N
 account.query/build.gradle.kts,d/c/dc51187cbb3052ccf5d3ce85a28ead3b9e09288d
 O
 account.common/build.gradle.kts,a/0/a0f2b85a257217e2db90525cdb01ab1e11f2ef9a
-L
-account.cmd/build.gradle.kts,5/1/51bc398d573808bd24599d1d2cf5190945cb98f0
--- bank-account/gradle/plugins/java-plugins/src/main/kotlin/my-java.gradle.kts
+++ bank-account/gradle/plugins/java-plugins/src/main/kotlin/my-java.gradle.kts
@@ -3,5 +3,7 @@ plugins {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+//    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
